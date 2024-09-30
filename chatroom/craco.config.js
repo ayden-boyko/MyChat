da47c11fd78cd12ec90f34cb7f6643add42d6104
Ayden Boyko <ajb7196@rit.edu>
2024-09-30 13:48:54 -0400

    backend

--- chatroom/craco.config.js
+++ chatroom/craco.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
   webpack: {
     alias: {
       "@lib": path.resolve(__dirname, "src/lib"),
